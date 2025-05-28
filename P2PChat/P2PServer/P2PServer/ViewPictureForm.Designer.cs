8e6482e3be453e779183be3be3ce95043d2fd3fb
A445121 <g51100526@gmail.com>
2025-05-28 20:27:11 +0800

    重新修改FORM名稱 開新資料夾存放

---
+++ P2PChat/P2PServer/P2PServer/ViewPictureForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace P2PServer
+{
+    partial class ViewPictureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnback = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(981, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16F);
+            this.btnsave.Location = new System.Drawing.Point(109, 546);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(193, 43);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "儲存圖片";
+            this.btnsave.UseVisualStyleBackColor = true;
+            // 
+            // btnback
+            // 
+            this.btnback.Font = new System.Drawing.Font("Microsoft JhengHei UI", 16F);
+            this.btnback.Location = new System.Drawing.Point(668, 546);
+            this.btnback.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(193, 43);
+            this.btnback.TabIndex = 1;
+            this.btnback.Text = "返回";
+            this.btnback.UseVisualStyleBackColor = true;
+            // 
+            // ViewPictureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.btnback);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ViewPictureForm";
+            this.Text = "查看圖片";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnsave;
+        private Button btnback;
+    }
+}
